f550c7b72cfb8d3d248f31aa42089a66bcfff92f
Juan José Méndez <juanjomendez029@gmail.com>
2020-07-24 13:05:50 -0500

    Se agrego quienes somos

---
+++ pages/quienes-somos.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center mt-3">Quienes Somos</h1>
+                <hr>
+            </div>
+            <!-- Row inicio -->
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <h3 class="text-center">Fundación</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione voluptas sint tempore dolore soluta ea doloribus quia quae. Voluptate iste esse molestiae temporibus nam accusamus ducimus quidem incidunt explicabo iure! Lorem ipsum,
+                        dolor sit amet consectetur adipisicing elit. Eaque eum nobis ea assumenda repellat dolorum pariatur ipsum animi expedita optio. Corporis necessitatibus et reiciendis rerum vitae nisi possimus. Voluptates, animi.</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <h3 class="text-center">Misión</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione voluptas sint tempore dolore soluta ea doloribus quia quae. Voluptate iste esse molestiae temporibus nam accusamus ducimus quidem incidunt explicabo iure! Lorem ipsum,
+                        dolor sit amet consectetur adipisicing elit. Eaque eum nobis ea assumenda repellat dolorum pariatur ipsum animi expedita optio. Corporis necessitatibus et reiciendis rerum vitae nisi possimus. Voluptates, animi.</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <h3 class="text-center">Equipo</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione voluptas sint tempore dolore soluta ea doloribus quia quae. Voluptate iste esse molestiae temporibus nam accusamus ducimus quidem incidunt explicabo iure! Lorem ipsum,
+                        dolor sit amet consectetur adipisicing elit. Eaque eum nobis ea assumenda repellat dolorum pariatur ipsum animi expedita optio. Corporis necessitatibus et reiciendis rerum vitae nisi possimus. Voluptates, animi.</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <h3 class="text-center">Visión</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione voluptas sint tempore dolore soluta ea doloribus quia quae. Voluptate iste esse molestiae temporibus nam accusamus ducimus quidem incidunt explicabo iure! Lorem ipsum,
+                        dolor sit amet consectetur adipisicing elit. Eaque eum nobis ea assumenda repellat dolorum pariatur ipsum animi expedita optio. Corporis necessitatibus et reiciendis rerum vitae nisi possimus. Voluptates, animi.</p>
+                </div>
+            </div>
+            <!-- Row final -->
+            <div class="w-100"></div>
+            <div class="col-6">
+                <img class="justify-content-center" src="img/Galactic.jpg" width="50%" height="100%" alt="Imagen Logo">
+            </div>
+            <div class="col-md-6">
+                <h2>Nuestro Origen</h2>
+                <p><strong>Galactic Devs</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis architecto unde ut temporibus dolorem harum minus perspiciatis, beatae ab nemo! Vel omnis excepturi eum earum facere molestiae quam, aperiam
+                    dolor.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ducimus a reiciendis eius quisquam alias ipsa. Facere at debitis nobis quos quasi error, quas consequatur temporibus nesciunt quidem voluptatibus rem.</p>
+            </div>
+        </div>
+        <div class="row justify-content-end">
+            <div class="col-6">
+                <div class="alert alert-warning mt-3">
+                    <strong>Aviso</strong> ipsum dolor sit amet consectetur adipisicing elit. <a href="#" class="alert-link"> Iure, aperiam.</a>
+                </div>
+            </div>
+        </div>
+        <div class="row contacto">
+            <div class="col text-center">
+                <h3>Contactenos</h3>
+            </div>
+            <div class="w-100"></div>
+            <div class="col-lg-6">
+                <form action="">
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre">
+                    </div>
+                    <div class="form-group">
+                        <label for="telefono">Telefono</label>
+                        <input type="tel" class="form-control" name="telefono" id="telefono" placeholder="Telefono">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="mensaje">Mensaje</label>
+                        <textarea name="mensaje" id="mensaje" class="form-control" placeholder="Escriba su mensaje"></textarea>
+                    </div>
+                    <div class="row justify-content-center justify-content-lg-end">
+                        <button type="button" class="btn col-3 btn-warning mb-3 mt-3">Enviar</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-6 contactenos">
+                <h4>Envie su Consulta o preguntenos</h4>
+                <div class="row justify-content-center">
+                    <button type="button" class="btn col-3 btn-warning mb-3 mt-3">Equipo</button>
+                    <div class="w-100"></div>
+                    <button type="button" class="btn col-3 btn-warning mb-3 mt-3">Equipo</button>
+                    <div class="w-100"></div>
+                    <button type="button" class="btn col-3 btn-warning mb-3 mt-3">Equipo</button>
+                </div>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
